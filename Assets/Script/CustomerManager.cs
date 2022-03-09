26dc5820f4c66119de014227cce7801122216151
mazikaaa <45326553+mazikaaa@users.noreply.github.com>
2022-03-09 19:55:18 +0900

    お客の処理をプログラミングするブランチの作成

---
+++ Assets/Script/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ お客に付けるスクリプト
+ 乗せて欲しいトッピングやかき混ぜて欲しい回数の情報をここにいれる
+ */
+
+
+public class CustomerManager : nattoList
+{
+
+    //載せて欲しいトッピングを格納
+    public List<Topping> liketopping;
+
+    //かき混ぜて欲しい回数を格納
+    public int mixcount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/nattoList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nattoList : MonoBehaviour
+{
+    //納豆の種類やトッピングの種類を格納するスクリプト
+
+    //納豆の種類
+    public enum Natto { Kotubu, Ootubu, Hikiwari };
+
+    //トッピングの種類
+    public enum Topping { Negi, Nori, Cheese };
+
+}
