bfc1269cbad7d680ee96e64138044797190ca471
weisswein <archeologistspeshil@gmail.com>
2022-03-10 16:39:33 +0900

    音を少しだけ
    GameOverとSampleSceneとDevelopSceneに入れています。

---
+++ Assets/AudioScript/SoundPlay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SoundPlay : MonoBehaviour
+{
+    [SerializeField] AudioSource source1;
+    [SerializeField] AudioSource source2;
+    [SerializeField] AudioSource source3;
+    [SerializeField] AudioClip clip1;
+    [SerializeField] AudioClip clip2;
+    [SerializeField] AudioClip clip3;
+    public UseRotateAround Countobj;
+    void Update()
+    {
+     if(Countobj.Cou == 10)
+        {
+            source1.PlayOneShot(clip2);
+        }
+     else if(Countobj.Cou == 30)
+        {
+            source2.PlayOneShot(clip3);
+        }
+     else if(Countobj.Cou == 1)
+        {
+            source3.PlayOneShot(clip1);
+        }
+    }
+}
