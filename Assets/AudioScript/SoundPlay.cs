3da21d2edf25e1e83ba0a2be1df6a362bce8658c
mazikaaa <45326553+mazikaaa@users.noreply.github.com>
2022-03-11 10:46:46 +0900

    納豆混ぜた時の効果音の修正

--- Assets/AudioScript/SoundPlay.cs
+++ Assets/AudioScript/SoundPlay.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 //このスクリプトは、効果音を動かします。
 public class SoundPlay : MonoBehaviour
 {
+    //音が重ならないようにするためフラグ
+    private bool clip1flag = false;
+    private bool clip2flag = false;
+
     // 音を調整するコンポーネントを取得
     [SerializeField] AudioSource source1;
     [SerializeField] AudioSource source2;
@@ -16,13 +20,20 @@ public class SoundPlay : MonoBehaviour
     public UseRotateAround Countobj;
     void Update()
     {
-     if(Countobj.Cou == 10)//10回混ぜたときに、効果音
+     if(Countobj.Cou == 10 && !clip1flag)//10回混ぜたときに、効果音
         {
             source1.PlayOneShot(clip1);
+            clip1flag = true;
         }
-     else if(Countobj.Cou == 30)//30回混ぜたときに、効果音
+     else if(Countobj.Cou == 30 && !clip2flag)//30回混ぜたときに、効果音
         {
             source2.PlayOneShot(clip2);
+            clip2flag = true;
+        }
+     else if(Countobj.Cou == 0)//初期状態(混ぜ無し状態)で効果音のフラグを戻す
+        {
+            clip1flag = false;
+            clip2flag = false;
         }
    
     }
